b0ec3279ebebd1d5abf239c25d3f57bfa838d94d
Bipeen <acharyab@seas.upenn.edu>
2018-12-29 20:31:30 -0800

    Login layout resource file + widgets for login activity.

--- app/src/main/java/com/example/redditapp/Account/LoginActivity.java
+++ app/src/main/java/com/example/redditapp/Account/LoginActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.redditapp.R;
 
@@ -11,10 +15,35 @@ public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
+    private ProgressBar mProgressBar;
+    private EditText mUsername;
+    private EditText mPassword;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Log.d(TAG, "onCreate: started");
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mUsername = (EditText) findViewById(R.id.input_username);
+        mProgressBar = (ProgressBar) findViewById(R.id.loginRequestLoadingProgressBar);
+        mProgressBar.setVisibility(View.GONE);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Attempting to log in.");
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                if(!username.equals("") && !password.equals("")) {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    // method for signing in
+                }
+            }
+        });
     }
 }
